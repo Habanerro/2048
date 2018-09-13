354e122570c31837814609bbb7ee01681701bf9a
Habanerro <fitohal@yandex.ru>
2018-09-14 00:44:43 +0700

    Add func of display_menu

---
+++ src/display_menu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+void display_menu () 
+{
+
+	printf("Внутриигровые команды:\n");
+	printf("2. Соеденить вниз\n");
+	printf("4. Соеденить влево\n");
+	printf("6. Соеденить вправо\n");
+	printf("8. Соеденить вверх\n\n");
+	printf("0. Начать новую игру\n");
+	printf("1. Завершить игру\n");
+	printf("3. Посмотреть рекорды\n");
+	printf("7. Шаг назад\n");
+	printf("9. Сохранить рекорд(завершить игру)\n");
+	printf("5. Открыть меню\n");
+	printf("Удобнее использовать numpad =)\n");
+}
